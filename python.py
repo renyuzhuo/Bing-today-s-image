a1dfc9d7181d75229c5043979edbdd147aa4183e
RenYuZhuo <renyuzhuo@foxmail.com>
2017-10-21 21:56:20 +0800

    20171021

--- python.py
+++ python.py
@@ -84,26 +84,26 @@ def getImage():
 	for pageNum in range(1, 51):
 		weburl = "http://bing.plmeizi.com/?page=" + str(pageNum)
 		page = requests.get(weburl)
-		my = MyHtmlParser('web/' + str(pageNum) + '.txt');
+		my = MyHtmlParser('/Users/ren/Desktop/GitHub/python/web/' + str(pageNum) + '.txt');
 		print(page.text)
 		my.feed(page.text)
-		with open('imageUrl/temp_' + str(pageNum) + '.txt', 'w') as f:
+		with open('/Users/ren/Desktop/GitHub/python/imageUrl/temp_' + str(pageNum) + '.txt', 'w') as f:
 			# f.write(str(images))
 			pass
 		print('-------------------')
 		# print(images)
 		print('-------------------')
 		# images=[]
-		# img_name = 'web/' + str(pageNum) + '.txt'
+		# img_name = '/Users/ren/Desktop/GitHub/python/web/' + str(pageNum) + '.txt'
 		# with open(img_name,'wb') as f:
 		# 	f.write(page.content)
 		jsonStr = json.dumps(list(imagesObj), default=oneDay2dict)
-		with open('all.json', 'w') as fJ:
+		with open('/Users/ren/Desktop/GitHub/python/all.json', 'w') as fJ:
 			fJ.write(jsonStr)
 		print('download successful: ' + str(pageNum))
 
 def downloadImage():
-	with open('all.json', 'r') as f:
+	with open('/Users/ren/Desktop/GitHub/python/all.json', 'r') as f:
 		imagesObj=json.load(f)
 	print(imagesObj)
 	for imageObjTemp in imagesObj:
@@ -112,14 +112,14 @@ def downloadImage():
 		imageWebUrl = imgUrl
 		imgUrl = imgUrl.replace('http://bimgs.plmeizi.com/images/bing/2017/', '').replace('http://bimgs.plmeizi.com/images/bing/2016/', '').replace('http://s.cn.bing.net/az/hprichbg/rb/', '')
 		img = requests.get(imageWebUrl)
-		with open('write/' + imgUrl, 'wb') as f:
+		with open('/Users/ren/Desktop/GitHub/python/write/' + imgUrl, 'wb') as f:
 			f.write(img.content)
 
 def getNowTime():
     return int(time.strftime("%Y%m%d",time.localtime(time.time())))
 
 def getImageFromBing():
-	with open('all.json', 'r') as f:
+	with open('/Users/ren/Desktop/GitHub/python/all.json', 'r') as f:
 		imagesObj=json.load(f)
 	# print(imagesObj[0]['data'])
 	imageData = int(imagesObj[0]['data'])
@@ -134,23 +134,23 @@ def getImageFromBing():
 	# print(jsonObj)
 	position = 0
 	for bingImageObj in jsonObj['images']:
-		print(bingImageObj)
+		# print(bingImageObj)
 		if int(bingImageObj['enddate']) > imageData:
 			oneDay = OneDay(bingImageObj['copyright'] + ' ('+ bingImageObj['enddate'] +')', 'http://www.bing.com' + bingImageObj['url'], bingImageObj['enddate'])
 			img = requests.get('http://www.bing.com' + bingImageObj['url'])
 			imgUrl = bingImageObj['url'].replace('/az/hprichbg/rb/', '')
-			with open('images/' + imgUrl, 'wb') as f:
+			with open('/Users/ren/Desktop/GitHub/python/images/' + imgUrl, 'wb') as f:
 				f.write(img.content)
 			imagesObj.insert(position, oneDay)
 			position = position + 1
 			jsonStr = json.dumps(list(imagesObj), default=oneDay2dict2)
-			with open('all.json', 'w') as fJ:
+			with open('/Users/ren/Desktop/GitHub/python/all.json', 'w') as fJ:
 				fJ.write(jsonStr)
 	os.system('git add .')
 	os.system('git commit -m "'+str(getNowTime())+'"')
 	os.system('git push origin master')
 
-if __name__ == "__main__":   
+if __name__ == "__main__":
     # getImage()
 	# downloadImage()
 	getImageFromBing()
